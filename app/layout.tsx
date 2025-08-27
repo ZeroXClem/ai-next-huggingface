db4a651293fca21ddb358e6f67a957f36db766fa
asimboss00789-blip <asimboss00789@gmail.com>
2025-08-27 03:22:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,52 +8,12 @@ export const metadata = {
   description: 'Powered by HuggingFace & Vercel'
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="font-bold text-3xl dark:text-white flex flex-col items-center justify-center h-screen">
-
-  <div className="w-3/4">
-
-    {messages.length > 0 &&
-
-      messages.map(m => (
-
-        <p key={m.id} className="break-words mb-2">
-
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-
-          {m.content}
-
-        </p>
-
-      ))
-
-    }
-
-  </div>
-
-  <form onSubmit={handleSubmit} className="w-3/4">
-
-    <input
-
-      className="p-4 mt-8 border border-gray-700 rounded-lg w-full"
-
-      value={input}
-
-      placeholder="Ask a question..."
-
-      onChange={handleInputChange}
-
-    />
-
-  </form>
-
-</div>
-
+    <html lang="en" className="dark">
+      <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
+        {children}
+      </body>
+    </html>
   )
-  }
-
+}
